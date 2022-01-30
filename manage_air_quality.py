d46343fb8aa3dc2f6fe557b9252e1f79d8bc59a1
Scott Owades <jeremyscottowades@gmail.com>
2022-01-30 12:23:04 -0800

    clean up logic quite a bit

--- manage_air_quality.py
+++ manage_air_quality.py
@@ -4,7 +4,7 @@ import json
 import requests
 import os
 
-SMOKY_TRESHOLD = 5
+AQI_CEILING = 50
 REFRESH_INTERVAL_MINS = 15
 REFRESH_INTERVAL_SECS = REFRESH_INTERVAL_MINS * 60
 AQI_URL = "https://www.purpleair.com/json?show="
@@ -28,11 +28,11 @@ HEADERS = { 'Content-Type' : 'application/json',
 
 def main(argv):
     aqi = get_aqi()
-    if(aqi >= SMOKY_TRESHOLD):
-        print(f'Current AQI of {str(aqi)} is above the acceptable value of {str(SMOKY_TRESHOLD)}, running fan for {str(REFRESH_INTERVAL_MINS)} min')
+    if(aqi >= AQI_CEILING):
+        print(f'Current AQI of {str(aqi)} is above the acceptable value of {str(AQI_CEILING)}, running fan for {str(REFRESH_INTERVAL_MINS)} min')
         set_fan_timer(REFRESH_INTERVAL_SECS)
     else:
-        print(f'Current AQI of {str(aqi)} is below the acceptable value of {str(SMOKY_TRESHOLD)}, will check again in {str(REFRESH_INTERVAL_MINS)} min')
+        print(f'Current AQI of {str(aqi)} is below the acceptable value of {str(AQI_CEILING)}, will check again in {str(REFRESH_INTERVAL_MINS)} min')
 
 def get_aqi():
     result = requests.get(AQI_URL + PURPLE_AIR_DEVICE_ID)
@@ -41,31 +41,21 @@ def get_aqi():
     return pm2_5_atm
 
 def set_mode(mode):
-    command = 'sdm.devices.commands.ThermostatMode.SetMode'
-    params = {
-        'mode' : mode
-    }
-    message = {
-        'command': 'sdm.devices.commands.ThermostatMode.SetMode',
-        'params': params
-    }
-    execute_thermostat_command(command, message)
+    params = {'mode' : mode}
+    execute_thermostat_command('SetMode', params)
 
 def set_fan_timer(duration_secs):
     turn_heat_on_if_off()
-    command = 'sdm.devices.commands.Fan.SetTimer'
-    params = {
-        'timerMode' : 'ON',
-        'duration' : str(duration_secs) + 's'
-    }
-    message = {
-        'command': command,
-        'params': params
-    }
-    execute_thermostat_command(command, message)
+    params = {'timerMode' : 'ON',
+              'duration' : str(duration_secs) + 's'
+            }
+    execute_thermostat_command('SetTimer', params)
 
-def execute_thermostat_command(command, message):
+def execute_thermostat_command(command, params):
     post_url = BASE_QUERY_URL + ':executeCommand'
+    message = {'command': f'sdm.devices.commands.Fan.{command}',
+                'params': params
+                }
 
     response = requests.post(post_url, headers=HEADERS, json=message)
     print(response.json())
